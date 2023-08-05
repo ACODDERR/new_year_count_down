4aed964b9c4a2228124184c082f243e436a28a09
unknown <amanwankhede98@gmail.com>
2023-08-05 18:27:36 +0530

    Initial commit

---
+++ countdown.js
@@ -0,0 +1,26 @@
+// Set the date for New Year
+const newYearDate = new Date("January 1, 2024 00:00:00").getTime();
+
+// Update the countdown every second
+const countdown = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = newYearDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+}, 1000);
